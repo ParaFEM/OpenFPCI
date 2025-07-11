0cc1023a99100ef2caa3393c814f5bee8fdc9bf2
jacthyli <123363791+jacthyli@users.noreply.github.com>
2025-07-11 16:21:42 +0100

    Update FPCI_install.sh

--- FPCI_install.sh
+++ FPCI_install.sh
@@ -27,35 +27,12 @@ sudo apt-get update && sudo apt-get install -y \
     wget \
     nano \
     tcsh \
-    curl \
     gnuplot \
     gnuplot-qt \
     gnuplot-data
 
-# sudo ln -s /usr/lib/x86_64-linux-gnu/libgfortran.so.5 /usr/lib/x86_64-linux-gnu/libgfortran.so
+
 # Set working directory
-curl -sSLO http://www.mpich.org/static/downloads/3.4.2/mpich-3.4.2.tar.gz
-sudo tar -xzf mpich-3.4.2.tar.gz -C /root
-sudo su
-cd /root/mpich-3.4.2
-make -j8 install
-rm -rf /root/mpich-3.4.2
-exit
-cd ~
-rm /mpich-3.4.2.tar.gz
-
-curl -sSLO http://mvapich.cse.ohio-state.edu/download/mvapich/osu-micro-benchmarks-5.4.1.tar.gz
-sudo tar -xzf osu-micro-benchmarks-5.4.1.tar.gz -C /root \
-sudo su
-cd /root/osu-micro-benchmarks-5.4.1
-./configure --prefix=/usr/local CC=/usr/bin/mpicc CXX=/usr/bin/mpicxx
-cd mpi
-make -j8 install
-rm -rf /root/osu-micro-benchmarks-5.4.1 \
-exit
-cd ~
-rm /osu-micro-benchmarks-5.4.1.tar.gz
-
 mkdir -p ~/foam
 cd ~/foam
 
@@ -68,21 +45,11 @@ echo 'export WM_THIRD_PARTY_USE_BISON_27=1' >> etc/prefs.sh
 echo "export WM_CC='gcc-5'"  >> etc/prefs.sh
 echo "export WM_CXX='g++-5'"  >> etc/prefs.sh
 echo "export PARAVIEW_SYSTEM=0" >> etc/prefs.sh
-echo "export WM_MPLIB='MPICH'" >> etc/prefs.sh
-echo "export MPI_ROOT='/usr'" >> etc/prefs.sh
-echo "export MPI_ARCH_FLAGS='-DMPICH_SKIP_MPICXX'" >> etc/prefs.sh
-echo "export MPI_ARCH_INC='-I${MPI_ROOT}/include'" >> etc/prefs.sh
-echo "export MPI_ARCH_LIBS='-L${MPI_ROOT}/lib${WM_COMPILER_LIB_ARCH} -L${MPI_ROOT}/lib -lmpich -lrt'" >> etc/prefs.sh
-echo "export LD_LIBRARY_PATH=$MPI_ROOT/lib:$LD_LIBRARY_PATH" >> etc/prefs.sh
-echo "export PATH=$MPI_ROOT/bin:$PATH" >> etc/prefs.sh
 
 # Modify OpenMPI spec file to enable Fortran support
-# cd ThirdParty/rpmBuild/SPECS
-# sed -i '143s/--disable-mpi-f90/--enable-mpi-f90/' openmpi-1.8.8.spec
-# sed -i '143 a\        FC=gfortran \\' openmpi-1.8.8.spec
-
-sed -i 's/: ${WM_MPLIB:=OPENMPI}; export WM_MPLIB/: ${WM_MPLIB:=MPICH}; export WM_MPLIB/' etc/bashrc
-sed -i 's/\[\ "$WM_MPLIB"\ !=\ "SYSTEMOPENMPI"\ ]/\[ "$WM_MPLIB" != "MPICH" \]/' ThirdParty/AllMake.stage2
+cd ThirdParty/rpmBuild/SPECS
+sed -i '143s/--disable-mpi-f90/--enable-mpi-f90/' openmpi-1.8.8.spec
+sed -i '143 a\        FC=gfortran \\' openmpi-1.8.8.spec
 
 # Uncomment lines in fvSchemes.C
 cd ~/foam/foam-extend-4.0/src/finiteVolume/finiteVolume/fvSchemes
@@ -105,25 +72,25 @@ source etc/bashrc
 mkdir -p $WM_PROJECT_USER_DIR
 mkdir -p $FOAM_RUN
 
-# # Install FSI module
-# cd $WM_PROJECT_USER_DIR
-# wget https://openfoamwiki.net/images/d/d6/Fsi_40.tar.gz
-# tar -xzf Fsi_40.tar.gz
+# Install FSI module
+cd $WM_PROJECT_USER_DIR
+wget https://openfoamwiki.net/images/d/d6/Fsi_40.tar.gz
+tar -xzf Fsi_40.tar.gz
 
-# # Fix RBFMeshMotionSolver
-# cd ./FluidSolidInteraction/src/fluidSolidInteraction/fluidSolvers/finiteVolume/RBFMeshMotionSolver/
-# sed -i '9 a #include <vector>' RBFMeshMotionSolver.C
+# Fix RBFMeshMotionSolver
+cd ./FluidSolidInteraction/src/fluidSolidInteraction/fluidSolvers/finiteVolume/RBFMeshMotionSolver/
+sed -i '9 a #include <vector>' RBFMeshMotionSolver.C
 
-# # Compile FSI module
-# cd $WM_PROJECT_USER_DIR/FluidSolidInteraction/src
-# source ~/foam/foam-extend-4.0/etc/bashrc
-# ./Allwmake > FSI_log.txt 2>&1
-# # Update include paths in options files
-# cd $WM_PROJECT_USER_DIR/FluidSolidInteraction
-# find run -name options | while read item; do
-#     sed -i -e 's=$(WM_PROJECT_DIR)/applications/solvers/FSI=$(WM_PROJECT_USER_DIR)/FluidSolidInteraction/src=' $item
-#     sed -i -e 's=$(WM_THIRD_PARTY_DIR)/packages/eigen3=$(WM_PROJECT_USER_DIR)/FluidSolidInteraction/src/ThirdParty/eigen3=' $item
-# done
+# Compile FSI module
+cd $WM_PROJECT_USER_DIR/FluidSolidInteraction/src
+source ~/foam/foam-extend-4.0/etc/bashrc
+./Allwmake > FSI_log.txt 2>&1
+# Update include paths in options files
+cd $WM_PROJECT_USER_DIR/FluidSolidInteraction
+find run -name options | while read item; do
+    sed -i -e 's=$(WM_PROJECT_DIR)/applications/solvers/FSI=$(WM_PROJECT_USER_DIR)/FluidSolidInteraction/src=' $item
+    sed -i -e 's=$(WM_THIRD_PARTY_DIR)/packages/eigen3=$(WM_PROJECT_USER_DIR)/FluidSolidInteraction/src/ThirdParty/eigen3=' $item
+done
 
 # Install ParaFEM
 cd ~
@@ -155,7 +122,7 @@ lib_path=$(find /usr/lib/gcc/x86_64-linux-gnu/ -name "libgfortran.so" | head -n 
 if [[ -n "$lib_path" ]]; then
     echo "Found libgfortran at: $lib_path"
     echo "Creating symlink in /usr/bin..."
-    
+
     sudo ln -sf "$lib_path" /usr/bin/libgfortran.so
 
     echo "Symlink created: /usr/bin/libgfortran.so -> $lib_path"
@@ -170,9 +137,4 @@ source ~/foam/foam-extend-4.0/etc/bashrc
 # Add alias for convenience
 echo "alias fe40='source ~/foam/foam-extend-4.0/etc/bashrc'" >> ~/.bashrc
 
-echo "export PATH=/usr/local/libexec/osu-micro-benchmarks/mpi/pt2pt:$PATH" >> ~/.bashrc
-echo "export PATH=/usr/local/libexec/osu-micro-benchmarks/mpi/collective:$PATH" >> ~/.bashrc
-echo "export LD_LIBRARY_PATH=/usr/lib/libibverbs:$LD_LIBRARY_PATH" >> ~/.bashrc
-
 echo "Installation Done!"
-
