8d604c9722ffec9c3d9c0fbb021a5bf476eaf75c
jacthyli <123363791+jacthyli@users.noreply.github.com>
2025-10-24 14:50:47 +0100

    Enable Fortran support in MPICH installation

--- FPCI_install.sh
+++ FPCI_install.sh
@@ -46,7 +46,7 @@ curl -sSLO http://www.mpich.org/static/downloads/3.4.2/mpich-3.4.2.tar.gz
 sudo tar -xzf mpich-3.4.2.tar.gz -C /root
 sudo su
 cd /root/mpich-3.4.2
-./configure --prefix=/usr --with-device=ch4:ofi --disable-fortran
+./configure --prefix=/usr --with-device=ch4:ofi --enable-fortran=all
 make -j8 install
 rm -rf /root/mpich-3.4.2
 exit
